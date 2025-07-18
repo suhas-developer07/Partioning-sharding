c39db7d656cf7639ac427c59e85975668fa131de
suhas-developer07 <sharmasuhas22@gmail.com>
2025-07-18 19:00:40 +0530

    completed

--- Routes/router.go
+++ Routes/router.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/suhas-developer07/Partioning-sharding/db"
@@ -99,7 +100,7 @@ func MountRoutes()*mux.Router{
 				})	 
 			 
 			 case Payload.Id<=50 :
-			 query := `INSERT INTO user_analytics_p4 (id,session_time,country) VALUES($1,$2,$3)`
+			 query := `INSERT INTO user_analytics_p5 (id,session_time,country) VALUES($1,$2,$3)`
 
 			 _,err := db.Shard2.Exec(query,Payload.Id,Payload.Session_Time,Payload.Country)
 			 if err!=nil{
@@ -116,6 +117,114 @@ func MountRoutes()*mux.Router{
 				})	
 		   }
 	}).Methods("POST")
+
+
+	router.HandleFunc("/{id}",func(w http.ResponseWriter, r *http.Request) {
+	 vars := mux.Vars(r)
+	 Id := vars["id"]
+	 log.Print(Id)
+
+	 userId ,_ :=strconv.Atoi(Id)
+
+	 log.Print(userId)
+
+	 var data Payload
+
+	 switch  {
+
+	 case userId<=10 :
+		query := `SELECT session_time,country FROM user_analytics_p1 WHERE id=$1`
+
+		err := db.Shard1.QueryRow(query,userId).Scan(
+			&data.Session_Time,
+			&data.Country,
+		)
+		if err!=nil {
+			w.WriteHeader(http.StatusInternalServerError)
+				json.NewEncoder(w).Encode(map[string]string{
+					"msg":"unable to get the data",
+					"error":err.Error(),
+				})
+				return 
+		}
+
+	     w.WriteHeader(http.StatusOK)
+		 json.NewEncoder(w).Encode(data)
+		 
+	case userId<=20 :
+		query := `SELECT session_time,country FROM user_analytics_p2 WHERE id=$1`
+
+		err := db.Shard1.QueryRow(query,userId).Scan(
+			&data.Session_Time,
+			&data.Country,
+		)
+		if err!=nil {
+			w.WriteHeader(http.StatusInternalServerError)
+				json.NewEncoder(w).Encode(map[string]string{
+					"msg":"unable to get the data",
+				})
+				return 
+		}
+
+	     w.WriteHeader(http.StatusOK)
+		 json.NewEncoder(w).Encode(data)	
+
+	 case userId<=30 :
+		query := `SELECT session_time,country FROM user_analytics_p3 WHERE id=$1`
+
+		err := db.Shard1.QueryRow(query,userId).Scan(
+			&data.Session_Time,
+			&data.Country,
+		)
+		if err!=nil {
+			w.WriteHeader(http.StatusInternalServerError)
+				json.NewEncoder(w).Encode(map[string]string{
+					"msg":"unable to get the data",
+				})
+				return 
+		}
+
+	     w.WriteHeader(http.StatusOK)
+		 json.NewEncoder(w).Encode(data)	
+	
+	 case userId<=40 :
+		query := `SELECT session_time,country FROM user_analytics_p4 WHERE id=$1`
+
+		err := db.Shard2.QueryRow(query,userId).Scan(
+			&data.Session_Time,
+			&data.Country,
+		)
+		if err!=nil {
+			w.WriteHeader(http.StatusInternalServerError)
+				json.NewEncoder(w).Encode(map[string]string{
+					"msg":"unable to get the data",
+				})
+				return 
+		}
+
+	     w.WriteHeader(http.StatusOK)
+		 json.NewEncoder(w).Encode(data)	
+
+	 case userId<=50 :
+		query := `SELECT session_time,country FROM user_analytics_p5 WHERE id=$1`
+
+		err := db.Shard2.QueryRow(query,userId).Scan(
+			&data.Session_Time,
+			&data.Country,
+		)
+		if err!=nil {
+			w.WriteHeader(http.StatusInternalServerError)
+				json.NewEncoder(w).Encode(map[string]string{
+					"msg":"unable to get the data",
+				})
+				return 
+		}
+
+	     w.WriteHeader(http.StatusOK)
+		 json.NewEncoder(w).Encode(data)	
+	 }
+
+	}).Methods("GET")
 
 
 
